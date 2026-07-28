b8055a70729219ec926b0d32e1658bf57c5e02d6
Yogesh Negi <yogesh.negi@wemonde.com>
2026-07-28 11:21:45 +0530

    feat: add credential configuration file and update email placeholder in login form

---
+++ creds/cred.js
@@ -0,0 +1,14 @@
+// NEW_URLS
+
+// MONGODB_URI=mongodb+srv://cellxchangecom_db_user:BURr4vaiCgdYdg8Y@cluster0.kf8gxdj.mongodb.net/cell_xchange
+// JWT_SECRET=super_secret_jwt_signkey_for_cell_xchange_12345
+// LOCAL_UPLOAD_DIR=uploads
+
+// CLOUDINARY_CLOUD_NAME=kdccn2v2
+// CLOUDINARY_API_KEY=186253899519217
+// CLOUDINARY_API_SECRET=AIVgJPej7cSqzLVP-_ah8D8soJw
+// CLOUDINARY_URL=cloudinary://186253899519217:AIVgJPej7cSqzLVP-_ah8D8soJw@kdccn2v2
+
+// ADMIN_NAME=Mobile Hub Owner
+// ADMIN_EMAIL=admin@cellxchange.in
+// ADMIN_PASSWORD="Mobile@12345"
